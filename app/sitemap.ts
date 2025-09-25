91505038a4ad22c320a9789aaa20c0f3884ed6fe
Cain <dong.yy1916@gmail.com>
2025-09-25 18:29:32 +0800

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
+      url:  process.env.NEXT_PUBLIC_SITE_URL,
       lastModified: new Date().toISOString(),
       changeFrequency: "daily",
       priority: 1,
